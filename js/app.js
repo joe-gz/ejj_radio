4edd7c2cd4869ff2ccd69d67de778b921cdcc876
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-12-16 14:47:37 -0500

    adds welcome

--- js/app.js
+++ js/app.js
@@ -4,6 +4,28 @@
   angular
   .module("radio", [
     "ui.router",
-    "songs"
+    "songs",
+    "welcome"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
   ]);
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("welcomeIndex",{
+      url:"/welcome",
+      templateUrl:"js/welcome/index.html",
+      controller:"WelcomeIndexController",
+      controllerAs:"WelcomeIndexViewModel"
+    })
+    // .state("songsIndex",{
+    //   url:"/songs",
+    //   templateUrl:"js/songs/index.html",
+    //   controller:"SongsIndexController",
+    //   controllerAs:"SongsIndexViewModel"
+    // })
+  }
+
 }())
